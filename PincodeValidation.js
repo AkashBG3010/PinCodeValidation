ad10b897995e977b5a506c3602aad1c7801c8087
Akash B G <akashbpura@gmail.com>
2022-02-01 20:12:50 +0530

    Added to validate Pincode entered

---
+++ PincodeValidation.js
@@ -0,0 +1,14 @@
+var pincode = prompt('Enter the required 6-digit pincode to be validated: ')
+const pincodeRegex = RegExp('^[0-9]{6}$')
+
+try {
+if (pincodeRegex.test(pincode)) {
+    console.log('Entered pincode is Valid!!')
+}
+else {
+    console.org0('Entered pincode is invalid!!')
+}
+}
+catch (e) {
+    console.error(e)
+}
